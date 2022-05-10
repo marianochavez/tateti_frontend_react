078246705af7d1f67dd3712cab371148044ad0b7
marianochavez <amarianochavez@gmail.com>
2022-05-10 12:24:14 -0300

    first commit

---
+++ tateti_frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import {Board} from "./components/Board";
+
+function App() {
+  return (
+    <div>
+      <h3 className="container">Ta - Te - Ti</h3>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
